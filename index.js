e5fac944392d1e6eb4262262cde18bdd43c1c24d
nicodemos234 <nicodemos54324@gmail.com>
2023-04-01 11:13:01 -0300

    Add param

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 import OBSWebSocket from 'obs-websocket-js';
+process.argv.forEach(function (val, index, array) {
+  if(index === 2) {
 
-const obs = new OBSWebSocket();
+    const obs = new OBSWebSocket();
 
-const asynch = async () => {
-  await obs.connect('ws://192.168.0.8:4455');
-  const res = await obs.call('SetCurrentProgramScene',
-    {sceneName: 'Valorant'})
-  console.log(res)
-  await obs.disconnect();
-}
+    const asynch = async () => {
+      await obs.connect('ws://192.168.0.8:4455');
+      const res = await obs.call('SetCurrentProgramScene',
+        {sceneName: val})
+      console.log(res)
+      await obs.disconnect();
+    }
 
-asynch();
+    asynch();
+  } 
+});
